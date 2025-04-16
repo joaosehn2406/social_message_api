526cbe02e4751dd02c058578def970b43fad4317
joaosehn2406 <jpssantosos5874@gmail.com>
2025-04-16 13:22:10 -0300

    User Entity

---
+++ src/main/java/com/jceco/redesocial_api/RedesocialApiApplication.java
@@ -0,0 +1,13 @@
+package com.jceco.redesocial_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedesocialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedesocialApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jceco/redesocial_api/RedesocialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jceco.redesocial_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedesocialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
