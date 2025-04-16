dacb8628d2a02330df465e7bf8415a453529f680
joaosehn2406 <jpssantosos5874@gmail.com>
2025-04-16 14:06:26 -0300

    PostRepository created

---
+++ src/main/java/com/jceco/redesocial_api/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.jceco.redesocial_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jceco.redesocial_api.entities.Post;
+
+public interface PostRepository extends JpaRepository<Post, String>{
+
+}
