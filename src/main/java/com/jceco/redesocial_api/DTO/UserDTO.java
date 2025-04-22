cddfc7a241dd8231148aac4c5531168aa8595b91
joaosehn2406 <jpssantosos5874@gmail.com>
2025-04-22 13:27:07 -0300

    Adjustments in project infrastructure

--- src/main/java/com/jceco/redesocial_api/DTO/UserDTO.java
+++ src/main/java/com/jceco/redesocial_api/DTO/UserDTO.java
@@ -1,14 +1,9 @@
 package com.jceco.redesocial_api.dto;
 
-import java.util.Set;
 
-<<<<<<< HEAD
+
 public record UserDTO(
     Long id,
     String name,
     String email
 ) {}
-=======
-public record UserDTO(Long id, String name, String email, Set<Long> postIds) {
-}
->>>>>>> c6d54c9ce2115a40b6fae4ace9780355586679e0
--- src/main/java/com/jceco/redesocial_api/entities/User.java
+++ src/main/java/com/jceco/redesocial_api/entities/User.java
@@ -5,48 +5,15 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-<<<<<<< HEAD
 @Entity
 @Table(name = "tb_user")
 public class User {
-=======
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "tb_user")
-public class User {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String name;
-	private String email;
-	
-	@OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
-	private Set<Post> posts;
-	
-	public User() {}
->>>>>>> c6d54c9ce2115a40b6fae4ace9780355586679e0
 
     @Id
     private Long id;
 
-<<<<<<< HEAD
     private String name;
     private String email;
-=======
-	public User(Long id, String name, String email, Set<Post> posts) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.posts = posts;
-	}
-
-	public void setPosts(Set<Post> posts) {
-		this.posts = posts;
-	}
->>>>>>> c6d54c9ce2115a40b6fae4ace9780355586679e0
 
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Post> posts = new HashSet<>();
@@ -78,7 +45,6 @@ public class User {
         this.name = name;
     }
 
-<<<<<<< HEAD
     public String getEmail() {
         return email;
     }
@@ -116,26 +82,4 @@ public class User {
     public int hashCode() {
         return Objects.hash(id);
     }
-=======
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	};
-
-
-
-
->>>>>>> c6d54c9ce2115a40b6fae4ace9780355586679e0
 }
